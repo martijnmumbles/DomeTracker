4648f7fc164b6e0f78f24c389931fba1b7180b23
Martijn Bloemheuvel <mbloemheuvel@iventmobile.nl>
2022-07-18 17:29:51 +0200

    report errors polling to discord

--- scripts/cron.py
+++ scripts/cron.py
@@ -1,6 +1,11 @@
 from apps.summoner.models import Summoner
+from discord_webhook import DiscordWebhook
+from django.conf import settings
 
 
 def run():
     for summ in Summoner.objects.all():
-        summ.poll()
+        try:
+            summ.poll()
+        except Exception as e:
+            DiscordWebhook.post_to_me(settings.DISCORD_ERROR_HOOK, e)
